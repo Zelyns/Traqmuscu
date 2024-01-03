9d5b6338107fea4d05497d5b5e9f883abca735e6
Etienne Rousseau <113974470+Zelyns@users.noreply.github.com>
2024-01-03 15:49:56 +0100

    Add files via upload

---
+++ traqmuscu/commandes.php
@@ -0,0 +1,24 @@
+<?php
+
+function ajouter($email, $mdp, $nom, $age, $poids ,$taille){
+    require("connexion.php"); 
+ 
+    
+    if(isset($access)) {
+        
+        $req = $access->prepare("INSERT INTO membres (email, mdp, nom, age, poids, taille) VALUES(:email, :mdp, :nom, :age, :poids, :taille)");
+        
+        $req->bindParam(':email', $email);
+        $req->bindParam(':mdp', $mdp);
+        $req->bindParam(':nom', $nom);
+        $req->bindParam(':age', $age);
+        $req->bindParam(':poids', $poids);
+        $req->bindParam(':taille', $taille);
+        
+        $req->execute();
+        $req->closeCursor();
+    } else {
+        
+        echo "Erreur de connexion à la base de données.";
+    }
+}
---
+++ traqmuscu/view/header_ex.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+  <img href="index.php" src="img/logo.png" alt="Logo de mon site" width="100" height="50">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="exercice.php">Back</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="chest.php">Chest</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="leg.php">Leg</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="arm.php">Arm</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="shoulder.php">Shoulder</a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+    
+</body>
+</html>
